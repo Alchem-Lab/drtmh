9bb1bf4953755b6fe1a8baccc1c8cc24f127b161
xxcclong <641568013@qq.com>
2019-10-31 18:12:57 +0800

    adapted protocols to use tcp

--- src/framework/bench_worker.cc
+++ src/framework/bench_worker.cc
@@ -309,6 +309,7 @@ void BenchWorker::run() {
 
 #if USE_TCP_MSG == 1
   assert(local_comm_queues.size() > 0);
+  assert(local_comm_queues.size() > worker_id_);
   create_tcp_connections(local_comm_queues[worker_id_],tcp_port,send_context);
 #else
   MSGER_TYPE type;
